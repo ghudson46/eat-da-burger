9015d702ee4d2d50dc32ea70d773c6b5f87aeda1
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-21 09:55:32 -0400

    created orm.js inside config directory and created orm for selectAll, insertOne, and updateOne

---
+++ config/orm.js
@@ -0,0 +1,30 @@
+const connection = require('./connection');
+
+const orm = {
+  selectAll: function() {
+    var queryString = "SELECT * FROM burgers;";
+    var query = connection.query(queryString, (err, result) => {
+      if (err) throw err;
+      console.log(query.sql);
+      console.log(result);
+    });
+  },
+  insertOne: function(burgerName, isDevoured) {
+    var queryString = "INSERT INTO ?? (burger_name, devoured) VALUES (?, ??);";
+    var query = connection.query(queryString, [table, burgerName, isDevoured], (err, result) => {
+      if (err) throw err;
+      console.log(query.sql);
+      console.log(result);
+    });
+  },
+  updateOne: function(id) {
+    var queryString = "UPDATE burgers SET devoured = NOT devoured WHERE id = ??;";
+    var query = connection.query(queryString, [id], (err, result) => {
+      if (err) throw err;
+      console.log(query.sql);
+      console.log(result);
+    });
+  }
+};
+
+module.exports = orm;
