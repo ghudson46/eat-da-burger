1a979c831889703a14428daf52f5a98dda4c3eb9
Garrett Hudson <garretthudson87@yahoo.com>
2020-09-21 10:21:18 -0400

    created burgers_controllers.js inside of controllers directory

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const router = express.router();
+
+
+const burger = require('../models/burger');
+
+
+router.get("/", (req, res) => {
+  burger.selectAll(data => {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne([
+    "burgerName", "isDevoured"
+  ], [
+    req.body.burgerName, req.body.isDevoured
+  ], result => {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", (req, res) => {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, result => {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+
+
+module.exports = router;
+
+// if bugs, could be with this file as well 
